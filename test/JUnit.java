c7ad192e71d7d623c26cf9981c7645ef05f63ed2
Akaime93 <Tamara.Gabasch@student.uibk.ac.at>
2015-04-27 13:19:44 +0200

    Added a allFacilities.html (not completed)

--- test/JUnit.java
+++ test/JUnit.java
@@ -9,16 +9,9 @@ import java.time.LocalDateTime;
  * Created by Tammy on 18.04.2015.
  */
 public class JUnit {
- /*
-    Activity activity = new Activity();
-
-    //DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm");
-    activity.from() = LocalDateTime.of(2014, 3, 30, 12, 30);
 
 
-    @Test
-
-*/
+    
 
 
 
