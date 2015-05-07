1eef0a28b44375e57220c5fce58d7362eb0194fc
kohloderso <christina.kohl@student.uibk.ac.at>
2015-05-07 13:13:05 +0200

    queryView form done

--- app/models/SportType.java
+++ app/models/SportType.java
@@ -1,9 +1,11 @@
 package models;
 
 import play.db.ebean.Model;
+import scala.collection.JavaConverters;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.List;
 
 /**
  * Created by Christina on 01.05.2015.
@@ -23,5 +25,10 @@ public class SportType extends Model {
             String.class, SportType.class
     );
 
+    public static scala.collection.immutable.List<SportType> allAsScalaList() {
+        List<SportType> sports = SportType.find.all();
+        scala.collection.immutable.List<SportType> ls = JavaConverters.asScalaBufferConverter(sports).asScala().toList();
+        return ls;
+    }
 
 }
