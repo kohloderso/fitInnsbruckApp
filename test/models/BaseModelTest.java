b44ea9666f921347223be1802da11a1a53aa42d6
Christina <Christina.Kohl@student.uibk.ac.at>
2015-06-15 00:09:23 +0200

    Tests ein wenig geändert

--- test/models/BaseModelTest.java
+++ test/models/BaseModelTest.java
@@ -20,36 +20,18 @@ import static org.junit.Assert.assertTrue;
 
 public class BaseModelTest {
   public static FakeApplication app;
-  public static String createDdl = "";
-  public static String dropDdl = "";
-  
+
   @BeforeClass
-  public static void startApp() throws IOException {
+  public static void startApp() {
     app = Helpers.fakeApplication(Helpers.inMemoryDatabase());
     Helpers.start(app);
-    
-    // Reading the evolution file
-    String evolutionContent = FileUtils.readFileToString(
-            app.getWrappedApplication().getFile("conf/evolutions/default/1.sql"));
- 
-    // Splitting the String to get Create & Drop DDL
-    String[] splittedEvolutionContent = evolutionContent.split("# --- !Ups");
-    String[] upsDowns = splittedEvolutionContent[1].split("# --- !Downs");
-    createDdl = upsDowns[0];
-    dropDdl = upsDowns[1];
-    
   }
- 
+
   @AfterClass
   public static void stopApp() {
     Helpers.stop(app);
   }
-  
-  @Before
-  public void createCleanDb() {
-    Ebean.execute(Ebean.createCallableSql(dropDdl));
-    Ebean.execute(Ebean.createCallableSql(createDdl));
-  }
+
 
   @Test
   public void testIsOpen() throws Exception{
