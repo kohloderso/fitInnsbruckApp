2a7e91cbaa3a30b1c06c99ea2adc412bc6794648
katix <katharina.bernal@student.uibk.ac.at>
2015-06-18 17:55:19 +0200

    updated

--- test/JUnit.java
+++ test/JUnit.java
@@ -1,9 +1,12 @@
 import models.Athlete;
+import models.Facility;
+import models.SportType;
 import org.fest.assertions.Assert;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import play.Logger;
 import play.api.libs.ws.WS;
 import play.libs.F;
 import play.mvc.Result;
@@ -23,6 +26,8 @@ import play.test.TestBrowser;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.*;
@@ -43,7 +48,7 @@ public class JUnit
     }
 
     @Test
-    public void Athlete_Test_getAge() {
+    public void Athlete_Test_getAge() throws Exception{
         Athlete athlete1= new Athlete();
         String dateInString = "06/06/1994";
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
@@ -59,18 +64,30 @@ public class JUnit
         assertEquals(21, athlete1.getAge());
     }
 
+    //checks if function findUser is right
     @Test
-    public void Athlete_Test_findUser(){
+    public void Athlete_Test_findUser()throws Exception {
         Athlete athlete2= new Athlete();
+        Athlete athlete1= new Athlete();
+        athlete1.name="emma";
         athlete2.name="jose";
-
+        athlete1.save();
         athlete2.save();
         String id_athlete= athlete2.findUser("jose").id;
         String id_athlete2 = athlete2.id;
-        assertEquals(id_athlete,id_athlete2);
+        assertEquals(id_athlete, id_athlete2);
     }
 
-
+    @Test
+    public void Athlete_Test_authenticate()throws Exception {
+        Athlete athlete3= new Athlete();
+        athlete3.name="hello";
+        athlete3.password="password";
+        athlete3.save();
+        String id_same = athlete3.authenticate("hello", "password").id;
+        String id_same2 = athlete3.id;
+        assertEquals(id_same,id_same2);
+    }
 
 
 /*
